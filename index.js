6819eefe7f6a1f1ec0b2914492ce32bc0a2f324a
Victor Wade <p2525657888@gmail.com>
2024-09-18 04:00:09 -1000

    roughing out the first function

--- index.js
+++ index.js
@@ -1,33 +1,57 @@
-const COHORT = "REPLACE_ME!";
+const COHORT = "2408-FTB-ET-WEB-FT";
 const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/artists`;
 
 // === State ===
 
 const state = {
-  artists: [],
+	artists: [],
 };
 
 /** Updates state with artists from API */
 async function getArtists() {
-  // TODO
+	// TODO
+	const response = await fetch(API_URL);
+
+	if (!response.ok) {
+		throw new Error(
+			`fn: getArtists: Error: ${response.status} ${response.statusText}`
+		);
+	}
+
+	// expect an array here? (or some kind of object)
+	const result = await response.json();
+
+	if (!result.success) {
+		throw new Error(`getArtists: API Error: ${result.error || "Unknown error"}`);
+	}
+
+	if (!Array.isArray(result.data)) {
+		throw new Error(`getArtists: API Error: Unexpected data format: Expected an Array`);
+	}
+
+	if (result.error) {
+		// do something on error, do not overwrite state
+	} else if (Array.isArray(result.data)) {
+		state.artists = result.data;
+	} else
 }
 
 /** Asks the API to create a new artist based on the given `artist` */
 async function addArtist(artist) {
-  // TODO
+	// TODO
 }
 
 // === Render ===
 
 /** Renders artists from state */
 function renderArtists() {
-  // TODO
+	// TODO
 }
 
 /** Syncs state with the API and rerender */
 async function render() {
-  await getArtists();
-  renderArtists();
+	await getArtists();
+	renderArtists();
 }
 
 // === Script ===
